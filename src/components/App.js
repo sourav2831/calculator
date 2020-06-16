ddd45735747e4496dc2972b27a49faa8e0813031
“sourav2831” <sourav2831@gmail.com>
2020-06-16 19:18:29 +0530

    Version 2.0

--- src/components/App.js
+++ src/components/App.js
@@ -5,21 +5,30 @@ import "../styles.css"
 function App() {
   const [inputText,setInputText]=useState("");
   const [ans,setAns]=useState(0);
+  const [check,setCheck]=useState(0);
   function handleClick(symbol){
     if(inputText==="Syntax Error!"){
       setAns(0);
       setInputText("");
     }
     if(symbols.includes(symbol,3)){
+      if(check!==1)
       setInputText((prevText)=>{
         return prevText+symbol;
       })
+      else if(symbol==='+' || symbol==='-' || symbol==='*' || symbol==='/'){
+      setInputText((prevText)=>{
+        return prevText+symbol;
+      })
+      setCheck(0);
+    }
     }
     else if(symbol===symbols[2]){
       try{
       const result=window.eval(inputText);
       setInputText(result);
       setAns(result);
+      setCheck(0);
       }
       catch{
         setInputText("Syntax Error!");
@@ -28,12 +37,20 @@ function App() {
     else if(symbol===symbols[0]){
       setAns(0);
       setInputText("");
+      setCheck(0);
     }
     else{
       setInputText((prevText)=>{
-        typeof prevText==="number" ? prevText=prevText+ans : 
-        prevText.charAt(prevText.length-1)==="/" || prevText.charAt(prevText.length-1)==="*" || prevText.charAt(prevText.length-1)==="+" || prevText.charAt(prevText.length-1)==="-" ? 
-        prevText=prevText+ans:prevText=prevText;
+        if(typeof prevText==="number"){
+          prevText=prevText+ans;
+          setCheck(1);
+      }
+      else{
+        if(prevText.charAt(prevText.length-1)==="/" || prevText.charAt(prevText.length-1)==="*" || prevText.charAt(prevText.length-1)==="+" || prevText.charAt(prevText.length-1)==="-"){ 
+        prevText=prevText+ans;
+        setCheck(1);
+        }
+      }
         return prevText;
       })
     }
